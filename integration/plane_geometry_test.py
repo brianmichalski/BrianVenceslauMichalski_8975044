887b6ed89875ecedb771752b8cf573f653f0ee9f
brianmichalski <90117970+brianmichalski@users.noreply.github.com>
2024-06-12 12:37:02 -0400

    fix: change the names of the parameters

--- integration/plane_geometry_test.py
+++ integration/plane_geometry_test.py
@@ -4,17 +4,17 @@ from pytest import approx
 
 # test cases for integration testing the combination of "plane_geometry.distance_between_points()"
 # and "plane_geometry.test_rectangle_area()" functions
-@pytest.mark.parametrize("width_line,height_line,area", [
+@pytest.mark.parametrize("width_coordinates,height_coordinates,area", [
     ([{ "x" : 2, "y": 4 }, { "x" : 12, "y": 4 }], [{ "x" : 12, "y": 4 }, { "x" : 12, "y": 0 }], 40),
 ])
-def test_distance_rectangle_area_from_drawing_points(width_line, height_line, area):
-    height = plane_geometry.distance_between_points(
-        x1=width_line[0]["x"], y1=width_line[0]["y"], 
-        x2=width_line[1]["x"], y2=width_line[1]["y"])
-    
+def test_distance_rectangle_area_from_drawing_points(width_coordinates, height_coordinates, area):
     width = plane_geometry.distance_between_points(
-        x1=height_line[0]["x"], y1=height_line[0]["y"], 
-        x2=height_line[1]["x"], y2=height_line[1]["y"])
+        x1=width_coordinates[0]["x"], y1=width_coordinates[0]["y"], 
+        x2=width_coordinates[1]["x"], y2=width_coordinates[1]["y"])
+    
+    height = plane_geometry.distance_between_points(
+        x1=height_coordinates[0]["x"], y1=height_coordinates[0]["y"], 
+        x2=height_coordinates[1]["x"], y2=height_coordinates[1]["y"])
     
     assert plane_geometry.rectangle_area(width, height) == area
 
@@ -24,10 +24,10 @@ def test_distance_rectangle_area_from_drawing_points(width_line, height_line, ar
     ([{ "x" : 2, "y": 4 }, { "x" : -4, "y": -1 }], 7.8102496, 1e-7),
     ([{ "x" : 0, "y": 0 }, { "x" : 0, "y": 0 }], 0, 0),
 ])
-def test_distance_between_points(points, distance, tolerance):
+def test_distance_between_points(coordinates, distance, tolerance):
     assert plane_geometry.distance_between_points(
-        x1=points[0]["x"], y1=points[0]["y"], 
-        x2=points[1]["x"], y2=points[1]["y"]) == approx(distance, abs=tolerance) 
+        x1=coordinates[0]["x"], y1=coordinates[0]["y"], 
+        x2=coordinates[1]["x"], y2=coordinates[1]["y"]) == approx(distance, abs=tolerance) 
 
 
 # test cases for unit testing of "plane_geometry.test_rectangle_area()" function
